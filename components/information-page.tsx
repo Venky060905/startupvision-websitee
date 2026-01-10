4705537f1972bef22c8e9a517954f589191d94c4
Developer <dev@example.com>
2026-01-10 14:46:46 +0530

    Update information page

--- components/information-page.tsx
+++ components/information-page.tsx
@@ -274,7 +274,7 @@ export default function InformationPage() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                   </svg>
                 </div>
-                <p className="text-gray-800 font-medium text-lg">1800-571-0809</p>
+                <p className="text-gray-800 font-medium text-lg">04045508829</p>
               </div>
               <div className="flex items-center gap-4">
                 <div className="w-12 h-12 rounded-full border-2 border-gray-300 flex items-center justify-center flex-shrink-0">
@@ -282,7 +282,7 @@ export default function InformationPage() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                   </svg>
                 </div>
-                <p className="text-gray-800 font-medium text-lg">info@startupflora.com</p>
+                <p className="text-gray-800 font-medium text-lg">info@startupvision.in</p>
               </div>
             </div>
             <button className="bg-green-500 hover:bg-green-600 text-white px-6 sm:px-8 py-2 sm:py-3 text-sm sm:text-base rounded-full font-semibold w-full sm:w-auto">
